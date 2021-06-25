49377d5e218b609732304e2282f6d262954920a4
Marcello <marcellocainelli@hotmail.com>
2021-06-25 10:21:05 -0300

    add entidade

---
+++ Source/Entidades/uFechaCaixa.pas
@@ -0,0 +1,80 @@
+unit uFechaCaixa;
+
+interface
+
+uses
+  Model.Entidade.Interfaces, Data.DB;
+
+Type
+  TFechaCaixa = class(TInterfacedObject, iEntidade)
+    private
+      FEntidadeBase: iEntidadeBase<iEntidade>;
+    public
+      constructor Create;
+      destructor Destroy; override;
+      class function New: iEntidade;
+      function EntidadeBase: iEntidadeBase<iEntidade>;
+      function Consulta(Value: TDataSource): iEntidade;
+      function InicializaDataSource(Value: TDataSource): iEntidade;
+      procedure ModificaDisplayCampos;
+  end;
+
+implementation
+
+uses
+  uEntidadeBase;
+
+{ TFechaCaixa }
+
+
+constructor TFechaCaixa.Create;
+begin
+  FEntidadeBase:= TEntidadeBase<iEntidade>.New(Self);
+  FEntidadeBase.TextoSQL('Select * from FECHACAIXA');
+end;
+
+destructor TFechaCaixa.Destroy;
+begin
+  inherited;
+end;
+
+class function TFechaCaixa.New: iEntidade;
+begin
+  Result:= Self.Create;
+end;
+
+function TFechaCaixa.EntidadeBase: iEntidadeBase<iEntidade>;
+begin
+  Result:= FEntidadeBase;
+end;
+
+function TFechaCaixa.Consulta(Value: TDataSource): iEntidade;
+var
+  vTextoSQL: string;
+begin
+  Result:= Self;
+  vTextoSQL:= FEntidadeBase.TextoSql;
+
+  Case FEntidadeBase.TipoPesquisa of
+    1: vTextoSQL:= vTextoSQL + ' Where DATA = :DATA AND COD_FILIAL = :pCOD_FILIAL';
+    2: vTextoSQL:= vTextoSQL + ' Where COD_CAIXA = :COD_CAIXA and DATA = :DATA and BAIXADO = :BAIXADO AND COD_FILIAL = :pCOD_FILIAL';
+  end;
+  FEntidadeBase.Iquery.IndexFieldNames('DATA');
+  FEntidadeBase.Iquery.SQL(vTextoSQL);
+  Value.DataSet:= FEntidadeBase.Iquery.Dataset;
+end;
+
+function TFechaCaixa.InicializaDataSource(Value: TDataSource): iEntidade;
+begin
+  Result:= Self;
+  FEntidadeBase.Iquery.IndexFieldNames('DATA');
+  FEntidadeBase.Iquery.SQL(FEntidadeBase.TextoSQL);
+  Value.DataSet:= FEntidadeBase.Iquery.Dataset;
+end;
+
+procedure TFechaCaixa.ModificaDisplayCampos;
+begin
+
+end;
+
+end.
