fc42c53ee8466208f2aa4a75161c640f074077ea
Marcello Cainelli Filho <marcellocainelli@hotmail.com>
2022-05-10 09:19:57 -0300

    nova entidade

---
+++ Source/Entidades/uPetRaca.pas
@@ -0,0 +1,86 @@
+unit uPetRaca;
+
+interface
+
+uses
+  Model.Entidade.Interfaces, Data.DB;
+
+Type
+  TPetRaca = class(TInterfacedObject, iEntidade)
+    private
+      FEntidadeBase: iEntidadeBase<iEntidade>;
+    public
+      constructor Create;
+      destructor Destroy; override;
+      class function New: iEntidade;
+      function EntidadeBase: iEntidadeBase<iEntidade>;
+      function Consulta(Value: TDataSource = nil): iEntidade;
+      function InicializaDataSource(Value: TDataSource = nil): iEntidade;
+      function DtSrc: TDataSource;
+      procedure ModificaDisplayCampos;
+  end;
+
+implementation
+
+uses
+  uEntidadeBase;
+
+{ TPetRaca }
+
+constructor TPetRaca.Create;
+begin
+  FEntidadeBase:= TEntidadeBase<iEntidade>.New(Self);
+  FEntidadeBase.TextoSQL('select * from PET_RACA where cod_especie = :pCod_Especie');
+
+  InicializaDataSource;
+end;
+
+destructor TPetRaca.Destroy;
+begin
+  inherited;
+end;
+
+class function TPetRaca.New: iEntidade;
+begin
+  Result:= Self.Create;
+end;
+
+function TPetRaca.EntidadeBase: iEntidadeBase<iEntidade>;
+begin
+  Result:= FEntidadeBase;
+end;
+
+function TPetRaca.Consulta(Value: TDataSource): iEntidade;
+begin
+  Result:= Self;
+  if Value = nil then
+    Value:= FEntidadeBase.DataSource;
+
+  FEntidadeBase.Iquery.IndexFieldNames('DESCRICAO');
+  FEntidadeBase.Iquery.SQL(FEntidadeBase.TextoSql);
+  Value.DataSet:= FEntidadeBase.Iquery.Dataset;
+end;
+
+function TPetRaca.InicializaDataSource(Value: TDataSource): iEntidade;
+var
+  vTextoSql: String;
+begin
+  Result:= Self;
+  if Value = nil then
+    Value:= FEntidadeBase.DataSource;
+  vTextoSql:= 'Select * From PET_RACA Where 1 <> 1';
+  FEntidadeBase.Iquery.SQL(vTextoSql);
+  Value.DataSet:= FEntidadeBase.Iquery.Dataset;
+end;
+
+procedure TPetRaca.ModificaDisplayCampos;
+begin
+
+end;
+
+function TPetRaca.DtSrc: TDataSource;
+begin
+  Result:= FEntidadeBase.DataSource;
+end;
+
+end.
