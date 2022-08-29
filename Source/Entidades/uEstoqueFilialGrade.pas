8f0b2ecfe899196bcd140e158d9e38ecc7183c72
Marcello Cainelli Filho <marcellocainelli@hotmail.com>
2022-08-29 14:48:03 -0300

    att entidade

--- Source/Entidades/uEstoqueFilialGrade.pas
+++ Source/Entidades/uEstoqueFilialGrade.pas
@@ -4,7 +4,6 @@ interface
 
 uses
   Model.Entidade.Interfaces, Data.DB, uLib, System.SysUtils;
-
 Type
   TEstoqueFilialGrade = class(TInterfacedObject, iEntidade)
     private
@@ -19,18 +18,22 @@ Type
       function DtSrc: TDataSource;
       procedure ModificaDisplayCampos;
   end;
-
 implementation
-
 uses
   uEntidadeBase;
-
+
+
 { TEstoqueFilialGrade }
 
 constructor TEstoqueFilialGrade.Create;
 begin
   FEntidadeBase:= TEntidadeBase<iEntidade>.New(Self);
-  FEntidadeBase.TextoSQL('select * from ESTOQUEFILIAL_GRADE where COD_FILIAL = :pCod_Filial and COD_PROD = :pCod_prod');
+  FEntidadeBase.TextoSQL(
+    'select efg.*, pt.descricao as tamanho, pc.descricao as cor ' +
+    'from estoquefilial_grade efg ' +
+    'left join produtos_tamanho pt on (pt.codigo = efg.cod_tamanho) ' +
+    'left join produtos_cores pc on (pc.codigo = efg.cod_cor) ' +
+    'where cod_filial = :pcod_filial and cod_prod = :pcod_prod ');
 end;
 
 destructor TEstoqueFilialGrade.Destroy;
