15f19640ba3213a3531bed52deb90d0f4314ee39
marcellocainelli <marcellocainelli@hotmail.com>
2025-07-07 09:29:25 -0300

    Create uConfigCashback.pas

---
+++ Source/Entidades/uConfigCashback.pas
@@ -0,0 +1,87 @@
+unit uConfigCashback;
+
+interface
+
+uses
+  Model.Entidade.Interfaces, Data.DB, System.SysUtils;
+
+Type
+  TConfigCashback = class(TInterfacedObject, iEntidade)
+    private
+      FEntidadeBase: iEntidadeBase<iEntidade>;
+    public
+      constructor Create;
+      destructor Destroy; override;
+      class function New: iEntidade;
+      function EntidadeBase: iEntidadeBase<iEntidade>;
+      function Consulta(Value: TDataSource = nil): iEntidade;
+      function InicializaDataSource(Value: TDataSource = nil): iEntidade;
+      function DtSrc: TDataSource;
+      procedure ModificaDisplayCampos;
+  end;
+
+implementation
+
+uses
+  uEntidadeBase;
+
+{ TConfigCashback }
+
+constructor TConfigCashback.Create;
+begin
+  FEntidadeBase:= TEntidadeBase<iEntidade>.New(Self);
+  FEntidadeBase.TextoSQL('Select * from CONFIG_CASHBACK where id = 1 ');
+
+  InicializaDataSource;
+end;
+
+destructor TConfigCashback.Destroy;
+begin
+  inherited;
+end;
+
+class function TConfigCashback.New: iEntidade;
+begin
+  Result:= Self.Create;
+end;
+
+function TConfigCashback.EntidadeBase: iEntidadeBase<iEntidade>;
+begin
+  Result:= FEntidadeBase;
+end;
+
+function TConfigCashback.Consulta(Value: TDataSource): iEntidade;
+var
+  vTextoSQL: string;
+begin
+  Result:= Self;
+  if Value = nil then
+    Value:= FEntidadeBase.DataSource;
+
+  FEntidadeBase.Iquery.IndexFieldNames('ID');
+  FEntidadeBase.Iquery.SQL(vTextoSQL);
+  Value.DataSet:= FEntidadeBase.Iquery.Dataset;
+end;
+
+function TConfigCashback.InicializaDataSource(Value: TDataSource): iEntidade;
+var
+  vTextoSql: String;
+begin
+  Result:= Self;
+  if Value = nil then
+    Value:= FEntidadeBase.DataSource;
+  vTextoSql:= 'Select * From CONFIG_CASHBACK Where 1 <> 1';
+  FEntidadeBase.Iquery.SQL(vTextoSql);
+  Value.DataSet:= FEntidadeBase.Iquery.Dataset;
+end;
+
+procedure TConfigCashback.ModificaDisplayCampos;
+begin
+end;
+
+function TConfigCashback.DtSrc: TDataSource;
+begin
+  Result:= FEntidadeBase.DataSource;
+end;
+
+end.
